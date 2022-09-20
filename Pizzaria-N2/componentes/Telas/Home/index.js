6b9fa8f978c9dd0e7a6940098bcf4030992d8ede
Pedro Knok <pedrokn9@gmail.com>
2022-09-20 19:17:27 -0300

    WIP: Inicio Layout

--- Pizzaria-N2/componentes/Telas/Home/index.js
+++ Pizzaria-N2/componentes/Telas/Home/index.js
@@ -1,5 +1,6 @@
 import { react, useState, useEffect  } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons, Entypo, MaterialIcons } from '@expo/vector-icons';
 import styles from './styles';
 
 import {
@@ -30,31 +31,36 @@ export default function Home({ navigation }) {
     return (
         <View style={styles.container}>
 
-            <Text style={styles.titulo}>Escolha uma tela</Text>
+            <Text style={styles.titulo}>No Ponto Pizzas</Text>
 
             <TouchableOpacity style={styles.botao}
                 onPress={() => navigation.navigate('Produtos')}>
                 <Text>Gerenciamento de Produtos</Text>
+                <MaterialIcons name="local-pizza" size={32} color="white" />
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.botao}
                 onPress={() => navigation.navigate('ProdutosVenda')}>
                 <Text>Gerenciamento de Vendas</Text>
+                <MaterialIcons name="add-task" size={32} color="white" />
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.botao}
                 onPress={() => navigation.navigate('Vendas')}>
-                <Text>Lista de Vendas</Text>
+                <Text>Minhas Vendas</Text>
+                <MaterialIcons name="attach-money" size={32} color="white" />
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.botao}
                 onPress={() => navigation.navigate('Carrinhos')}>
                 <Text>Carrinho</Text>
+                <Ionicons name="cart" size={32} color="white" />
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.botao}
                 onPress={() => navigation.navigate('Categorias')}>
                 <Text>Gerenciamento de Categorias</Text>
+                <MaterialIcons name="category" size={32} color="white" />
             </TouchableOpacity>
 
         </View>
